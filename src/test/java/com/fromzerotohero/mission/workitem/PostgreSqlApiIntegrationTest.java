af522bf26b214afc1e6342641c7f8f0f90daa35e
jmmmdv <j.mmmdv@gmail.com>
2026-06-25 06:41:14 -0400

    Update PostgreSQL migration version test

--- src/test/java/com/fromzerotohero/mission/workitem/PostgreSqlApiIntegrationTest.java
+++ src/test/java/com/fromzerotohero/mission/workitem/PostgreSqlApiIntegrationTest.java
@@ -47,7 +47,7 @@ class PostgreSqlApiIntegrationTest {
         String version = jdbc.queryForObject(
                 "select version from flyway_schema_history where success order by installed_rank desc limit 1",
                 String.class);
-        assertThat(version).isEqualTo("8");
+        assertThat(version).isEqualTo("9");
 
         Integer safetyColumns = jdbc.queryForObject("""
                 select count(*) from information_schema.columns
